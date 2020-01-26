3d29aed2eaddd7a8317dde95786a8c1f38d6a4f8
cadecameron <cade.cameron@gmail.com>
2020-01-25 21:16:07 -0800

    UPDATED minor formatting to variable name.

--- server/database/index.js
+++ server/database/index.js
@@ -16,10 +16,11 @@ db.on('open', () => console.log('Successfully connected to MongoDB!'));
 
 // Function that retrieves 25 reviews from the database given a productId parameter
 // and pagination information
-let getReviewsForProductId = (productId, pageOptions, cb) => {
+// Reference: https://stackoverflow.com/questions/5539955/how-to-paginate-with-mongoose-in-node-js
+let getReviewsForProductId = (product_id, pageOptions, cb) => {
   console.log(`About to execute a query for productId: ${productId}, page: ${pageOptions.page}, limit: ${pageOptions.limit}`);
   Reviews
-    .find({ product_id: productId })
+    .find({ product_id })
     .skip(pageOptions.page * pageOptions.limit)
     .sort({ date_created: -1 })
     .limit(pageOptions.limit)
