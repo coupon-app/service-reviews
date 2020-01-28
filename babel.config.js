cfd51dbd3fd33f3c4fd327eb58bd4d6b678d7c3c
cadecameron <cade.cameron@gmail.com>
2020-01-27 19:22:38 -0800

    UPDATED npm packages, config files and created sample tests for Jest/Enzyme environment.

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-react',
+  ];
+
+  const plugins = [
+    'add-module-exports',
+  ];
+
+  return {
+    presets,
+    plugins,
+  };
+};
---
+++ testing/samplesnapshot.spec.js
@@ -0,0 +1,20 @@
+/* SAMPLE TEST FOR JEST CONFIG */
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+
+import Reviews from '../client/src/Reviews';
+
+describe('Reviews Component', () => {
+  test('[Sample] snapshot renders', () => {
+    const component = renderer.create(<Reviews />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('[Sample] state should include reviews state property', () => {
+    const wrapper = mount(<Reviews />);
+    expect(wrapper.find('h2').text()).toEqual('Customer Reviews');
+  });
+});
