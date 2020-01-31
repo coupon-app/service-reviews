e6ce43a50285808a999ca999efe4bc4e2cd3e417
cadecameron <cade.cameron@gmail.com>
2020-01-30 17:44:41 -0800

    UPDATED test files to add more tests.

--- __tests__/Reviews.spec.jsx
+++ __tests__/Reviews.spec.jsx
@@ -11,12 +11,30 @@ describe('Reviews Component', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  test('Reviews Component shoud include correct heading 3 title text', () => {
+  test('Should include correct heading title text', () => {
     const wrapper = shallow(<Reviews />);
-    expect(wrapper.find('Reviews__Title').text()).toEqual('Customer Reviews');
+    expect(wrapper.find('Reviews__Title').text()).toContain('Customer Reviews');
   });
 
   test('Should contain a VerifiedReviewsHeader component', () => {
     const wrapper = shallow(<Reviews />);
+    expect(wrapper.find('VerifiedReviewsHeader')).toHaveLength(1);
+  });
+
+  test('Should contain a ReviewsContainer component', () => {
+    const wrapper = shallow(<Reviews />);
+    expect(wrapper.find('ReviewsContainer')).toHaveLength(1);
+  });
+
+  test('Should notify the user if there are no reviews, and not render review components', () => {
+    const wrapper = shallow(<Reviews />);
+    wrapper.setState({
+      reviews: null,
+      average: null,
+      ratings: null,
+    });
+    expect(wrapper.find('div').text()).toContain('No reviews for this product yet');
+    expect(wrapper.find('ReviewsContainer')).toHaveLength(0);
+    expect(wrapper.find('VerifiedReviewsHeader')).toHaveLength(0);
   });
 });
