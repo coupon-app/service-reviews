58cdabe511a9146f1396baba0eca855f2595d526
cadecameron <cade.cameron@gmail.com>
2020-02-07 17:49:51 -0800

    UPDATED server to use compression.

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
+const compression = require('compression');
 
 const db = require('../database');
 
@@ -20,6 +21,7 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.static(DIR_PUBLIC));
+app.use(compression());
 
 // ///////////////////////////////////////////////////////////////////////////////////////
 // ROUTES ////////////////////////////////////////////////////////////////////////////////
